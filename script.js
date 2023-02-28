cfc3032b8ed4e3b1c7823175aa4301e37eeb7dfb
James Pierce <james59222@gmail.com>
2023-02-27 20:36:48 -0600

    debugged characters they do not need to be separated

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // Variables for all password characters and numbers that can be used
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var mega = []
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "("]
-var password = ""
+
+var numbers =["0123456789"];
+var mega = [];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["!@#$%^&*()_+~\\`|}{[]:;?><,./-="];
+var password = " ";
 
 
 //Prompts for password length and whether to use uppercase, lowercase, numeric or special characters.
 function generatePassword() {
   var userChoiceNumber = window.prompt("Choose a password length min 8 max 128");
   //need logic for 8-128 if passes no 7 or 129
+  
   var upcase = window.confirm("Include Uppercase Letters?");
   var lowcase = window.confirm("Include Lowercase Letters?");
   var numeric = window.confirm("Include Numbers?");
   var specialCharacters = window.confirm("Include Special Characters?")
-
+  
 
 //logic if statements for choices
   if (numeric === true) {
@@ -24,15 +26,15 @@ function generatePassword() {
 
   if (upcase === true) {
     mega = mega + uppercase;
-  }
+    }
+
   if (lowcase === true) {
     mega = mega + lowercase;
-  }
+    }
 
   if (specialCharacters === true) {
     mega = mega + special;
-    
-  }
+    }
 //code for determining password
   for (let index = 0; index < userChoiceNumber; index++) {
    const random = Math.floor(Math.random() * mega.length);
@@ -43,8 +45,7 @@ function generatePassword() {
   mega=[];
   return password;
   
-  //mega=[];
-
+ 
 }
 
 
